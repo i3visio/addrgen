6e726ec630be0ca3cc4138145f8d3a8d8a76b950
i3visio <contacto@i3visio.com>
2015-01-21 17:57:59 +0100

    Version update.

--- i3_addrgen.py
+++ i3_addrgen.py
@@ -256,7 +256,7 @@ if __name__ == "__main__":
     # About options
     groupAbout = parser.add_argument_group('About arguments', 'Showing additional information about this program.')
     groupAbout.add_argument('-h', '--help', action='help', help='shows this help and exists.')
-    groupAbout.add_argument('--version', action='version', version='%(prog)s v0.1.0', help='shows the version of the program and exists.')
+    groupAbout.add_argument('--version', action='version', version='%(prog)s v0.1.1', help='shows the version of the program and exists.')
 
     args = parser.parse_args()	
 
